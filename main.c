cc0efe3bf5be60e3fdc1c247f397157bdad0ad0f
Neven Boric <nboric@gmail.com>
2023-03-20 15:00:32 -0700

    Print board before predicting next move, allow to accept suggestion

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include "common.h"
 #include "move_logic.h"
 #include "init.h"
@@ -51,19 +52,46 @@ void print_board(struct board* board)
 	printf("\n");
 }
 
-struct move_coord get_input()
-{
-	struct move_coord result;
 
+enum input_result
+{
+	INPUT_ERROR = -1,
+	INPUT_MANUAL_MOVE = 0,
+	INPUT_AUTO_MOVE = 1
+};
+
+int get_input(struct move_coord* move)
+{
+	char line[1024];
+	if (fgets(line, sizeof line, stdin) == NULL)
+	{
+		return INPUT_ERROR;
+	}
+	int n = strlen(line);
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (!isspace(line[i]))
+		{
+			break;
+		}
+	}
+	if (i == n)
+	{
+		return INPUT_AUTO_MOVE;
+	}
 	char c1, c2;
 	char i1, i2;
-    c1 = c2 = i1 = i2 = 0;
-	scanf(" %c%c%c%c", &c1, &i1, &c2, &i2);
+	c1 = c2 = i1 = i2 = 0;
+	if (sscanf(line, " %c%c%c%c", &c1, &i1, &c2, &i2) != 4)
+	{
+		return INPUT_ERROR;
+	}
 
-	result.from = (struct coord){ toupper(c1), i1 };
-	result.to = (struct coord){ toupper(c2), i2 };
+	move->from = (struct coord){ toupper(c1), i1 };
+	move->to = (struct coord){ toupper(c2), i2 };
 
-	return result;
+	return INPUT_MANUAL_MOVE;
 }
 
 int main()
@@ -77,15 +105,31 @@ int main()
 
 	while (!game_status.ended)
 	{
-		struct move_coord best_move = predict_best_move(&game_status);
-		printf("Hint: I would play %c%c->%c%c\n", best_move.from.c[0], best_move.from.c[1], best_move.to.c[0], best_move.to.c[1]);
+		int first = 1;
+		struct move_coord best_move;
 		int move_valid = 0;
 		struct move_coord move;
 		while (!move_valid)
 		{
 			print_board(board);
-			printf("Player %s, enter move:\n", game_status.current_player == WHITE ? "white" : "black");
-			move = get_input();
+			if (first)
+			{
+				first = 0;
+				best_move = predict_best_move(&game_status);
+			}
+			printf("Hint: I would play %c%c->%c%c\n", best_move.from.c[0], best_move.from.c[1], best_move.to.c[0], best_move.to.c[1]);
+			printf("Player %s, enter move, or press enter to accept suggested move:\n", game_status.current_player == WHITE ? "white" : "black");
+			enum input_result result = get_input(&move);
+			if (result == INPUT_ERROR)
+			{
+				continue;
+			}
+			if (result == INPUT_AUTO_MOVE)
+			{
+				printf("Playing suggested move\n");
+				move = best_move;
+			}
+			printf("Move is: %c%c->%c%c\n", move.from.c[0], move.from.c[1], move.to.c[0], move.to.c[1]);
 			move_valid = validate_move(&game_status, board, &move);
 		}
 		move_piece(board, &move);
